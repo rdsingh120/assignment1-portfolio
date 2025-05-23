38f2555b0a18cf9b4e130e09bc4382994b8d5e93
Ripudaman Singh <rdsingh120@gmail.com>
2025-05-24 01:07:46 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,8 @@
 const Footer = () => {
-  return <div className="bg-black p-5 text-[#f9f9f9] text-center">Footer</div>
+  return (
+    <div className="bg-black p-5 text-[#f9f9f9] text-center">
+      Copyright © 2025 Ripudaman Singh
+    </div>
+  )
 }
 export default Footer
